55b34eff9fabb3f09c8de92bf64dd04d8f6ee05f
Filip Lazarevic <filip.lazarevic984@gmail.com>
2024-05-28 18:59:15 +0200

    Refactor Code (with ChatGPT)

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -12,72 +12,64 @@ class Shop {
   }
 
   updateQuality() {
-    for (var i = 0; i < this.items.length; i++) {
-      if (
-        this.items[i].name != 'Aged Brie' &&
-        this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
-      ) {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            if (this.items[i].name.includes('Conjured')) {
-              this.items[i].quality = Math.max(0, this.items[i].quality - 2);
-            } else {
-              this.items[i].quality = this.items[i].quality - 1;
-            }
-          }
-        }
+    this.items.forEach((item) => {
+      switch (item.name) {
+        case 'Aged Brie':
+          this.updateAgedBrie(item);
+          break;
+        case 'Backstage passes to a TAFKAL80ETC concert':
+          this.updateBackstagePasses(item);
+          break;
+        case 'Sulfuras, Hand of Ragnaros':
+          // Sulfuras does not change in quality or sellIn
+          break;
+        default:
+          this.updateRegularItem(item);
+          break;
+      }
+      this.updateSellIn(item);
+    });
+    return this.items;
+  }
+
+  updateAgedBrie(item) {
+    if (item.quality < 50) {
+      item.quality += 1;
+    }
+    if (item.sellIn <= 0 && item.quality < 50) {
+      item.quality += 1;
+    }
+  }
+
+  updateBackstagePasses(item) {
+    let toAdd = 1;
+    if (item.sellIn <= 5) toAdd = 3;
+    else if (item.sellIn <= 10) toAdd = 2;
+    if (item.sellIn == 0) item.quality = 0;
+    else {
+      item.quality = Math.min(item.quality + toAdd, 50);
+    }
+  }
+
+  updateRegularItem(item) {
+    if (item.name.includes('Conjured')) {
+      item.quality = Math.max(0, item.quality - 2);
+    } else {
+      item.quality = Math.max(0, item.quality - 1);
+    }
+    if (item.sellIn <= 0) {
+      if (item.name.includes('Conjured')) {
+        item.quality = Math.max(0, item.quality - 2);
       } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (
-            this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert'
-          ) {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (
-            this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
-          ) {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                if (this.items[i].name.includes('Conjured')) {
-                  this.items[i].quality = Math.max(
-                    0,
-                    this.items[i].quality - 2,
-                  );
-                } else {
-                  this.items[i].quality = this.items[i].quality - 1;
-                }
-              }
-            }
-          } else {
-            this.items[i].quality =
-              this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
+        item.quality = Math.max(0, item.quality - 1);
       }
     }
+  }
 
-    return this.items;
+  updateSellIn(item) {
+    if (item.name !== 'Sulfuras, Hand of Ragnaros') {
+      item.sellIn -= 1;
+    }
   }
 }
 
