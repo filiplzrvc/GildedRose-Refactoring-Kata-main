ee3c8e5449e7a0629977d517535014c7a1a24f84
Filip Lazarevic <filip.lazarevic984@gmail.com>
2024-05-28 17:20:27 +0200

    Test 10 failed

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -19,55 +19,62 @@ describe('Gilded Rose', function () {
     expect(items[0].quality).toEqual(1);
   });
 
-    it('should update quality and sellIn for Elixir of the Mongoose', function () {
-      const gildedRose = new Shop([new Item('Elixir of the Mongoose', 5, 7)]);
-      const items = gildedRose.updateQuality();
-      expect(items[0].sellIn).toEqual(4);
-      expect(items[0].quality).toEqual(6);
-    });
-  
-    it('should not change quality or sellIn for Sulfuras, Hand of Ragnaros', function () {
-      const gildedRose = new Shop([
-        new Item('Sulfuras, Hand of Ragnaros', 0, 80),
-      ]);
-      const items = gildedRose.updateQuality();
-      expect(items[0].sellIn).toEqual(0);
-      expect(items[0].quality).toEqual(80);
-    });
-  
-    it('should not change quality or sellIn for Sulfuras, Hand of Ragnaros with negative sellIn', function () {
-      const gildedRose = new Shop([
-        new Item('Sulfuras, Hand of Ragnaros', -1, 80),
-      ]);
-      const items = gildedRose.updateQuality();
-      expect(items[0].sellIn).toEqual(-1);
-      expect(items[0].quality).toEqual(80);
-    });
-  
-    it('should update quality and sellIn for Backstage passes to a TAFKAL80ETC concert with more than 10 days', function () {
-      const gildedRose = new Shop([
-        new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
-      ]);
-      const items = gildedRose.updateQuality();
-      expect(items[0].sellIn).toEqual(14);
-      expect(items[0].quality).toEqual(21);
-    });
-  
-    it('should update quality and sellIn for Backstage passes to a TAFKAL80ETC concert with 10 days or less', function () {
-      const gildedRose = new Shop([
-        new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
-      ]);
-      const items = gildedRose.updateQuality();
-      expect(items[0].sellIn).toEqual(9);
-      expect(items[0].quality).toEqual(50);
-    });
-  
-    it('should update quality and sellIn for Backstage passes to a TAFKAL80ETC concert with 5 days or less', function () {
-      const gildedRose = new Shop([
-        new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
-      ]);
-      const items = gildedRose.updateQuality();
-      expect(items[0].sellIn).toEqual(4);
-      expect(items[0].quality).toEqual(50);
-    });
+  it('should update quality and sellIn for Elixir of the Mongoose', function () {
+    const gildedRose = new Shop([new Item('Elixir of the Mongoose', 5, 7)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toEqual(4);
+    expect(items[0].quality).toEqual(6);
+  });
+
+  it('should not change quality or sellIn for Sulfuras, Hand of Ragnaros', function () {
+    const gildedRose = new Shop([
+      new Item('Sulfuras, Hand of Ragnaros', 0, 80),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toEqual(0);
+    expect(items[0].quality).toEqual(80);
+  });
+
+  it('should not change quality or sellIn for Sulfuras, Hand of Ragnaros with negative sellIn', function () {
+    const gildedRose = new Shop([
+      new Item('Sulfuras, Hand of Ragnaros', -1, 80),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toEqual(-1);
+    expect(items[0].quality).toEqual(80);
+  });
+
+  it('should update quality and sellIn for Backstage passes to a TAFKAL80ETC concert with more than 10 days', function () {
+    const gildedRose = new Shop([
+      new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toEqual(14);
+    expect(items[0].quality).toEqual(21);
+  });
+
+  it('should update quality and sellIn for Backstage passes to a TAFKAL80ETC concert with 10 days or less', function () {
+    const gildedRose = new Shop([
+      new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toEqual(9);
+    expect(items[0].quality).toEqual(50);
+  });
+
+  it('should update quality and sellIn for Backstage passes to a TAFKAL80ETC concert with 5 days or less', function () {
+    const gildedRose = new Shop([
+      new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toEqual(4);
+    expect(items[0].quality).toEqual(50);
+  });
+
+  it('should update quality and sellIn for Conjured Mana Cake', function () {
+    const gildedRose = new Shop([new Item('Conjured Mana Cake', 3, 6)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toEqual(2);
+    expect(items[0].quality).toEqual(4);
+  });
 });
