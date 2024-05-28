7324743f78df8331ae75f62b859db7e6fc3ba091
Filip Lazarevic <filip.lazarevic984@gmail.com>
2024-05-28 17:12:29 +0200

    Test 8 with OK

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -52,4 +52,13 @@ describe('Gilded Rose', function () {
       expect(items[0].sellIn).toEqual(14);
       expect(items[0].quality).toEqual(21);
     });
+  
+    it('should update quality and sellIn for Backstage passes to a TAFKAL80ETC concert with 10 days or less', function () {
+      const gildedRose = new Shop([
+        new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
+      ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(9);
+      expect(items[0].quality).toEqual(50);
+    });
 });
