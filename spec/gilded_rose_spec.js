207e44b87ccba7067d1b10bf5c31afde63e95a6c
Filip Lazarevic <filip.lazarevic984@gmail.com>
2024-05-28 17:06:39 +0200

    Test 2 with OK

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,9 +1,14 @@
-describe("Gilded Rose", function() {
-
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+describe('Gilded Rose', function () {
+  it('should foo', function () {
+    const gildedRose = new Shop([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("foo");
+    expect(items[0].name).toEqual('foo');
   });
 
+  it('should update quality and sellIn for +5 Dexterity Vest', function () {
+    const gildedRose = new Shop([new Item('+5 Dexterity Vest', 10, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toEqual(9);
+    expect(items[0].quality).toEqual(19);
+  });
 });
